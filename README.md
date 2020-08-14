b76188ac09323eb047fce687834b3c2c745f6499
Mike <2872578+mikebywaters@users.noreply.github.com>
2020-08-15 00:27:08 +1000

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# dotfiles
+
+`git clone https://github.com/mikebywaters/dotfiles.git && cd dotfiles && ./install`
---
+++ brew-cask.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+# Let's get these puppies into the Applications directory.
+export HOMEBREW_CASK_OPTS="--appdir=/Applications"
+
+# Install Caskroom
+brew tap caskroom/cask
+brew tap buo/cask-upgrade
+brew install brew-cask-completion
+brew tap caskroom/versions
+
+brew cask outdated # List outdated casks.
+
+# Install packages
+apps=(
+    # Browsers
+    firefox
+    google-chrome
+    phantomjs
+
+    # Automation/Productivity/Team Tools.
+    alfred
+    harvest
+    flux
+    transmission
+    the-unarchiver
+    micro-snitch
+    spectacle
+    shift
+
+    # Password managers
+    lastpass
+    1password
+
+    # Developer Tools (Editors/IDE/Terminal)
+    github
+    vscodium
+    iterm2
+    transmit # SFTP & Amazon S3 File Transfer
+    virtualbox
+    vagrant
+    vagrant-manager # VM's
+    insomnia
+    tableplus
+
+    # Media Players/Convertors.
+    calibre # eBook conversion/management | https://manual.calibre-ebook.com/generated/en/cli-index.html
+    vlc # Best video player.
+
+    # Music
+    spotify
+    splice
+    
+    # Chats
+    whatsapp
+    goofy
+    microsoft-teams
+    zoomus
+    slack
+
+    #linktree
+    pritunl
+    zeplin
+    tuple
+    )
+
+for application in ${apps[@]}
+do
+    brew cask install $application
+done
+
+for application in ${apps[@]}
+do
+    brew install caskroom/cask/$application
+done
+
+brew cu -afy # Force ugprade of all packages.
